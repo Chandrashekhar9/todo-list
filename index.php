cd3898d3e1f80c26c2ea60d991d2a3324d7a84c6
Chandrashekhar9 <ace.cs99@gmail.com>
2020-03-25 22:58:27 +0530

    This is first commit with blank pages(Just for testing, for seeing that all things are working correctly so that i can do my work further.)

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>My&minus;Note</title>
+  </head>
+  <body>
+    <div class="container" style="background-color: red; height: 100vh;">
+        <div class="row justify-content-md-center">
+            <div class="col-md-8 col-12" style="background-color: blue; height: 50vh;"></div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/jquery-3.4.1.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Custom JS -->
+    <script src="assets/js/script.js"></script>
+  </body>
+</html>
